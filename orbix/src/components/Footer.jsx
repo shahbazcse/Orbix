d6e5d1ec2f44401d7c09e959de9acf9c81ba1a11
shahbazcse <cseshahbaz@gmail.com>
2023-08-26 12:16:46 +0530

    Refactored and Redesigned NavBar and Footer

--- orbix/src/components/Footer.jsx
+++ orbix/src/components/Footer.jsx
@@ -1,78 +1,67 @@
+import logo from "../assets/logo.png";
+
 export default function Footer() {
   return (
     <footer class="text-gray-600 body-font font-barlow text-2xl">
       <div class="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
         <div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left md:mt-0 mt-10">
-          <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              class="w-10 h-10 text-white p-2 bg-black border rounded-full"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-            </svg>
-            <span class="ml-3 text-xl">Footer</span>
-          </a>
-          <p class="mt-2 text-md text-gray-500">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At, rem.
-          </p>
+          <div className="flex gap-2 items-center">
+            <img className="h-10 w-10" src={logo} alt="Orbix" />
+            <p className="text-5xl font-mont font-bold text-gray-200">Orbix</p>
+          </div>
+          <p class="mt-2 text-md text-gray-200">Unveiling Cosmic Wonders.</p>
         </div>
         <div class="flex-grow flex flex-wrap md:pr-20 -mb-10 md:text-left text-center order-first">
           <div class="lg:w-1/4 md:w-1/2 w-full px-4">
             <h2 class="title-font font-medium text-md text-white tracking-widest mb-3">
-              CATEGORIES
+              COMPANY
             </h2>
             <nav class="list-none mb-10">
               <li>
                 <a class="text-gray-400 hover:text-gray-500 cursor-pointer">
-                  First Link
+                  About us
                 </a>
               </li>
               <li>
                 <a class="text-gray-400 hover:text-gray-500 cursor-pointer">
-                  Second Link
+                  Gallery
                 </a>
               </li>
               <li>
                 <a class="text-gray-400 hover:text-gray-500 cursor-pointer">
-                  Third Link
+                  Contact us
                 </a>
               </li>
               <li>
                 <a class="text-gray-400 hover:text-gray-500 cursor-pointer">
-                  Fourth Link
+                  Media Inquiries
                 </a>
               </li>
             </nav>
           </div>
           <div class="lg:w-1/4 md:w-1/2 w-full px-4">
             <h2 class="title-font font-medium text-md text-white tracking-widest mb-3">
-              CATEGORIES
+              EXPLORE
             </h2>
             <nav class="list-none mb-10">
               <li>
                 <a class="text-gray-400 hover:text-gray-500 cursor-pointer">
-                  First Link
+                  Accessibility
                 </a>
               </li>
               <li>
                 <a class="text-gray-400 hover:text-gray-500 cursor-pointer">
-                  Second Link
+                  Resources
                 </a>
               </li>
               <li>
                 <a class="text-gray-400 hover:text-gray-500 cursor-pointer">
-                  Third Link
+                  Subscribe
                 </a>
               </li>
               <li>
                 <a class="text-gray-400 hover:text-gray-500 cursor-pointer">
-                  Fourth Link
+                  FAQ
                 </a>
               </li>
             </nav>
--- orbix/src/components/NavBar.jsx
+++ orbix/src/components/NavBar.jsx
@@ -1,8 +1,14 @@
+import logo from "../assets/logo.png";
+
 export default function NavBar() {
   return (
     <div className="flex sticky top-0 z-10 justify-between items-center font-bold font-mont pl-8 pr-12 text-white drop-shadow-2xl opacity-[0.85] bg-black border-b-[1px] border-[#EEF1F5] py-4 ">
-      <div className="cursor-pointer tracking-[0.2rem] font-serif text-3xl">
-        Orbix
+      <div className="flex justify-center items-center gap-2 cursor-pointer tracking-[0.2rem] font-serif text-3xl">
+        <img className="h-10 w-10" src={logo} alt="Orbix" />
+        <p>Orbix</p>
+      </div>
+      <div className="text-2xl font-thin tracking-widest">
+        Unveiling Cosmic Wonders
       </div>
       <div className="cursor-pointer">
         <svg
